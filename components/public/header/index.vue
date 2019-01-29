780cde15028654946aa39b669f222901e5d39cf3
ningliu <ningliu@sf-express.com>
2019-01-29 14:43:25 +0800

    add searchbar

--- components/public/header/index.vue
+++ components/public/header/index.vue
@@ -1,33 +1,32 @@
 <template>
-  <el-row :gutter="0" class="m-header">
-    <el-col :span="4">
-      <Geo/>
-    </el-col>
-    <el-col :span="6">
-      <User/>
-    </el-col>
-    <el-col :span="14">
-      <Nav/>
-    </el-col>
-  </el-row>
+  <div class="m-header">
+    <el-row>
+      <el-col>
+        <topBar/>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <searchBar/>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-import Geo from "./geo";
-import User from "./user";
-import Nav from "./nav";
+import topBar from "./topbar.vue";
+import searchBar from "./searchbar.vue";
 
 export default {
   components: {
-    Geo,
-    User,
-    Nav
+    topBar,
+    searchBar
   }
 };
 </script>
 
 <style lang="scss">
-@import "@/assets/css/public/header.scss";
+@import "@/assets/css/public/header/index.scss";
 </style>
 
 
