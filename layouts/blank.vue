a91a9edf971d0cee52427c2c15d41ef43163f82d
ningliu <ningliu@sf-express.com>
2019-01-30 18:09:29 +0800

    update register

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,16 +1,6 @@
 <template>
-  <div class="page-register">
-    <article class="header">
-      <header>
-        <a href="/" class="site-logo"></a>
-        <span class="login">
-          <em class="bold">已有美团账号</em>
-          <a href="/login">
-            <el-button type="primary" size="small">登录</el-button>
-          </a>
-        </span>
-      </header>
-    </article>
+  <div class="layout-blank">
+    <nuxt/>
   </div>
 </template>
 
@@ -19,7 +9,6 @@ export default {};
 </script>
 
 <style lang="scss">
-@import "@/assets/css/register/index.scss";
 </style>
 
 
